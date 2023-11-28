1630b9af216cf3fc7ad7f366f47dbce649e91e9a
Paulo Vítor <paulovmedeiros@outlook.com>
2023-11-28 10:52:45 -0300

    concluindo capítulo

--- criando-exibindo-dados/src/App.vue
+++ criando-exibindo-dados/src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ titulo }}</h1>
+  <h1>{{ subtitulo }}</h1>
+
+  <input :disabled="tarefas.length == 0" v-model="subtitulo" type="text" placeholder="Descrição">
+
+  <div v-if="tarefas.length <= 0">
+    Não há tarefas.
+  </div>
+
+  <div v-if="tarefas.length > 0">
+    Existem {{ tarefas.length }} tarefas pendentes.
+    <ul>
+      <li v-for="(tarefa, id) in tarefas" :key="id"> {{ tarefa }} </li>
+    </ul>
+  </div>
+
+  <button @click="tarefas = []">Limpar</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'lv-tarefas',
+  data  () {
+    return {
+      titulo: 'Listagens de tarefas',
+      subtitulo: 'Defina uma descrição',
+      tarefas: []
+    }
+  },
+
+  mounted() {
+    setTimeout( () => {
+      this.titulo = "novo título"
+      this.tarefas = ['tarefa 1', 'tarefa 2', 'tarefa 3', 'tarefa 4']
+    }, 3000)
+  },
 }
 </script>
 
